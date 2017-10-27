fdb0e8245667f16d30bf0ec294ffea1813be75c2
Mike Geyser <mikegeyser@gmail.com>
2017-10-27 13:16:03 +0200

    Got the tracker animation working.

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ app/src/app/tracker/tracker.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+declare const d3: any;
+
+const d = {
+  x0: 250,
+  y0: 55,
+  yMax: 450,
+  range: 30,
+  animation: 1000
+};
+
+@Component({
+  selector: 'app-tracker',
+  templateUrl: './tracker.component.html',
+  styleUrls: ['./tracker.component.css']
+})
+export class TrackerComponent implements OnInit {
+  data = new BehaviorSubject(0);
+
+  constructor() { }
+
+  ngOnInit() {
+    // Generate random data
+    setInterval(_ => this.data.next(Math.random() * d.range), 5000);
+
+    const child = d3.select('.child');
+
+    const linearScale = d3.scaleLinear()
+      .domain([0, d.range])
+      .range([d.y0, d.yMax]);
+
+    this.data.subscribe(point => {
+      const y = linearScale(point);
+      child.transition()
+        .duration(d.animation).attr('y', y);
+    });
+  }
+
+  calculateDistance(rssi) {
+    const txPower = -59; // hard coded power value. Usually ranges between -59 to -65
+
+    if (rssi === 0) {
+      return -1.0;
+    }
+
+    const ratio = rssi * 1.0 / txPower;
+    if (ratio < 1.0) {
+      return Math.pow(ratio, 10);
+    }
+
+    const distance = (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
+    return distance;
+  }
+
+}
